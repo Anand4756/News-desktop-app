c3e54eb6ef94a0b8b80ceec1f6ddfecf42ab647e
Anand <73114463+Anand4756@users.noreply.github.com>
2021-12-02 15:45:21 +0530

    initial

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const https = require("https");
+const axios = require("axios");
 
 
 const app = express();
@@ -17,12 +20,30 @@ app.use(express.static("public"));
 
 
 app.get("/", async (req, res)=>{
-  // const url = 'https://newsapi.org/v2/everything?q=india&apiKey=';
-  // await https.get(url,(response)=>{
-    res.render('home');
-  //   console.log(response);
-  // })
-})
+const apiKey = process.env.API_KEY;
+try{
+// var url = 'http://newsapi.org/v2/top-headlines?' +
+//           'country=in&' +
+//           'apiKey='+apiKey;
+
+//         const news_get =await axios.get(url)
+//         console.log(news_get);
+//         res.render('home',{articles:news_get.data.articles})
+        res.render('home');
+  console.log("hello");
+  // https.get(url, (response)=>{
+  //   response.on("data",(data)=>{
+  //     const newsdata = JSON.parse(data);
+  //     res.render('home');
+} catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+
+    }
+
+
+});
 
 
 
