2336ea2c1b0037585b5ac4d69ac4f5869d4f65a3
Anand <73114463+Anand4756@users.noreply.github.com>
2021-12-05 18:03:19 +0530

    added cricket-news section in news

--- app.js
+++ app.js
@@ -26,9 +26,12 @@ var url = 'http://newsapi.org/v2/top-headlines?' +
           'country=in&' +
           'apiKey='+apiKey;
 
+var sports_news = 'http://newsapi.org/v2/everything?q=cricket&' +
+          'apiKey='+apiKey;
+
         const news_get =await axios.get(url)
-        
-        res.render('home',{articles:news_get.data.articles.slice(2,-1), articles2: news_get.data.articles[0],articles3: news_get.data.articles[1]})
+        const sports_get = await axios.get(sports_news);
+        res.render('home',{articles:news_get.data.articles.slice(2,-1), articles2: news_get.data.articles[0],articles3: news_get.data.articles[1], cricket:sports_get.data.articles})
         // res.render('home');
   console.log("hello");
   // https.get(url, (response)=>{
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.on('ready', () => {
 	window = new BrowserWindow({
 		width: 800,
 		height: 600,
+		autoHideMenuBar: true,
 		webPreferences: {
 			nodeIntegration: true
 		}
